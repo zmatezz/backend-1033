a25d001d5e73b87880f986fe94c18931fabff514
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-11-13 23:29:43 -0300

    feat: added get email route

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -1,15 +1,19 @@
-'use strict'
-const express = require('express');
+"use strict";
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/user-controller');
-const gameController = require('../controllers/game-controller');
-const identityService = require('../services/identity-service');
+const controller = require("../controllers/user-controller");
+const gameController = require("../controllers/game-controller");
+const identityService = require("../services/identity-service");
 
-
-router.get('/:id',controller.findById);
-router.post('/', controller.post);
-router.post('/login', controller.login);
-router.put('/:id', identityService.authorize, controller.put);
-router.get('/:userId/recommendations', identityService.authorize, gameController.getRecommendationsByUserId);
+router.get("/email/:email", controller.findByEmail);
+router.get("/:id", controller.findById);
+router.post("/", controller.post);
+router.post("/login", controller.login);
+router.put("/:id", identityService.authorize, controller.put);
+router.get(
+  "/:userId/recommendations",
+  identityService.authorize,
+  gameController.getRecommendationsByUserId
+);
 
 module.exports = router;
